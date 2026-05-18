84d44e9bdbc9c894079c80063ea074937a3c5ece
aryaa-001 <pankajp9282@gmail.com>
2026-05-18 20:23:44 +0530

    change

--- learnlog-backend/src/utils/sendEmail.js
+++ learnlog-backend/src/utils/sendEmail.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,

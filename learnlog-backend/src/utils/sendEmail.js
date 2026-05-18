aeda5804c11184271f16de4b4e2744c7477055b0
aryaa-001 <pankajp9282@gmail.com>
2026-05-18 20:19:33 +0530

    change

--- learnlog-backend/src/utils/sendEmail.js
+++ learnlog-backend/src/utils/sendEmail.js
@@ -19,4 +19,12 @@ const sendEmail = async ({ to, subject, text }) => {
   });
 };
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("Email configuration error:", error);
+  } else {
+    console.log("Email server is ready");
+  }
+});
+
 module.exports = sendEmail;

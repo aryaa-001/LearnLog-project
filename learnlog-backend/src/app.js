ba8e958ea25fe6320d18dd1b88ef4d4123673ba8
aryaa-001 <pankajp9282@gmail.com>
2026-05-18 18:54:24 +0530

    change

--- learnlog-backend/src/app.js
+++ learnlog-backend/src/app.js
@@ -2,25 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const entryRouter = require("./routes/entry.routes");
 const authRouter = require("./routes/auth.routes");
-const dashboardRouter = require("./routes/dashboard.routes")
-const profileRouter = require("./routes/profile.routes")
-const cookieParser = require('cookie-parser');
+const dashboardRouter = require("./routes/dashboard.routes");
+const profileRouter = require("./routes/profile.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use("/uploads", express.static("uploads"));
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://vishal-learnlog.vercel.app",
+  "https://learn-log-project.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/entries", entryRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/dashboard", dashboardRouter)
-app.use('/api/profile-update', profileRouter)
+app.use("/api/dashboard", dashboardRouter);
+app.use("/api/profile-update", profileRouter);
 
 module.exports = app;
